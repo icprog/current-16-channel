8d056088bccc6aba6c25ed9d944f277e7dafd875
imjustadog <imjustadog@outlook.com>
2017-05-18 10:18:05 +0800

    backup

---
+++ AD7176.c
@@ -0,0 +1,425 @@
+/***************************************************************************//**
+ *   @file   Communication.c
+ *   @brief  Implementation of Communication Driver.
+ *   @author DBogdan (dragos.bogdan@analog.com)
+********************************************************************************
+ * Copyright 2012(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: 812
+*******************************************************************************/
+
+/******************************************************************************/
+/* Include Files                                                              */
+/******************************************************************************/
+#include "AD7176.h"
+#if defined(__dsPIC33F__)
+#include <p33Fxxxx.h>
+#elif defined(__PIC24H__)
+#include <p24hxxxx.h>
+#endif
+
+/***************************************************************************//**
+ * @brief Initializes the SPI communication peripheral.
+ *
+ * @param lsbFirst - Transfer format (0 or 1).
+ *                   Example: 0x0 - MSB first.
+ *                            0x1 - LSB first.
+ * @param clockFreq - SPI clock frequency (Hz).
+ *                    Example: 1000 - SPI clock frequency is 1 kHz.
+ * @param clockPol - SPI clock polarity (0 or 1).
+ *                   Example: 0x0 - Idle state for clock is a low level; active
+ *                                  state is a high level;
+ *	                      0x1 - Idle state for clock is a high level; active
+ *                                  state is a low level.
+ * @param clockEdg - SPI clock edge (0 or 1).
+ *                   Example: 0x0 - Serial output data changes on transition
+ *                                  from idle clock state to active clock state;
+ *                            0x1 - Serial output data changes on transition
+ *                                  from active clock state to idle clock state.
+ *
+ * @return status - Result of the initialization procedure.
+ *                  Example: 1 - if initialization was successful;
+ *                           0 - if initialization was unsuccessful.
+*******************************************************************************/
+unsigned char SPI_Init()
+{
+    // Add your code here.
+	TRISGbits.TRISG6 = 0;
+	TRISGbits.TRISG7 = 1;
+	TRISGbits.TRISG8 = 0;
+	
+    TRISGbits.TRISG15 = 0;//CS1
+    TRISCbits.TRISC1 = 0;//CS2
+    TRISDbits.TRISD5 = 0;//CS3
+    TRISDbits.TRISD4 = 0;//CS4
+	
+    IFS2bits.SPI2IF = 0; // Clear the Interrupt Flag
+	IEC2bits.SPI2IE = 0; // Disable the Interrupt
+    SPI2STATbits.SPIROV=0;
+	// SPI1CON1 Register Settings
+	SPI2CON1bits.PPRE = 4; // 4:1 primary prescale
+	SPI2CON1bits.SPRE = 7; // 1:1 secondary prescale
+	SPI2CON1bits.DISSCK = 0; // Internal Serial Clock is Enabled
+	SPI2CON1bits.DISSDO = 0; // SDOx pin is controlled by the module
+	SPI2CON1bits.MODE16 = 0; // Communication is byte-wide (8 bits)
+	SPI2CON1bits.SMP = 1; // 
+	SPI2CON1bits.CKE = 0;// 
+	SPI2CON1bits.CKP = 1; // Idle state for clock is a low level; 
+	// active state is a high level
+	SPI2CON1bits.MSTEN = 1; // Master mode Enabled
+	SPI2STATbits.SPIEN = 1; // Enable SPI module
+	//SPI2BUF = 0x0000; // Write data to be trantesmitted 
+	// Interrupt Controller Settings
+	
+	nSS1 = 1;
+	nSS2 = 1;
+	nSS3 = 1;
+	nSS4 = 1;
+
+
+
+}
+
+unsigned char  read_write_byte(unsigned char data)
+{
+	unsigned char temp=0;
+	
+	asm("nop");
+	while(SPI2STATbits.SPITBF){asm("nop");}
+	SPI2BUF = data;
+	asm("nop");
+	
+	while(!SPI2STATbits.SPIRBF){asm("nop");}
+	temp= SPI2BUF;
+	return temp;
+}
+
+/*****空为reserved 0*******/
+void AD7176set(void)
+{
+	int i;
+
+	/*****
+	 AD 模式寄存器
+	bit 15     0 disable REF_EN
+	bit 13     0 sing_cyc
+	bit 10 :8  111  1ms
+	bit 6  :4  000 continual
+	bit 3  :2  10  xtal2 
+	
+	0x01	
+	************/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x01);
+		read_write_byte(0x07);   
+		read_write_byte(0x08);
+		cs_high(i);
+		DELAY(100); 
+	}
+	
+	/*************
+	借口模式寄存器
+	bit  12  0  alt-sync
+	bit  11  0  iostrength
+	bit  8   1  dout-reset
+	bit  7   0  contread
+	bit  6   1  data-stat
+	bit  5   0  reg-check
+	bit  3:2  00  crc-en
+	bit  0   0  wl16  
+		
+	0x02
+	**********/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x02);
+		read_write_byte(0x01);
+		read_write_byte(0x40);
+		cs_high(i);
+		DELAY(100); 
+	}
+	
+	/*************
+	寄存器检查
+	bit 23 0  
+		
+	0x03
+	**********/
+		
+	/*************
+	数据
+	bit 23 0  
+		
+	0x04
+	**********/
+		
+	/*************
+	gpio
+	bit 15 0  
+	
+	0x06
+	**********/
+		
+	/*************
+	ID
+	bit 15 0  	
+	
+	0x07
+	**********/
+	
+	/*************
+	通道映射 寄存器0 
+	bit 15      0    ch-en
+	bit 13 12   000   setup-sel0
+	bit 9  5    00000  ain0
+	bit 4  0    10110  ref- 
+	
+	0x10
+	**********/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x10);
+		read_write_byte(0x80);
+		read_write_byte(0x16);
+		cs_high(i);
+		DELAY(100); 
+	}
+	
+	/*************
+	通道映射 寄存器1
+	bit 15      0    ch-en
+	bit 13 12   000   setup-sel0
+	bit 9  5    00001  ain1
+	bit 4  0    10110  ref- 
+	
+	0x11
+	**********/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x11);
+		read_write_byte(0x80);
+		read_write_byte(0x36);
+		cs_high(i);
+		DELAY(100); 
+	}
+	
+	/*************
+	通道映射 寄存器2
+	bit 15      0    ch-en
+	bit 13 12   000   setup-sel0
+	bit 9  5    00000  ain2
+	bit 4  0    10110  ref- 
+	
+	0x12
+	**********/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x12);
+		read_write_byte(0x80);
+		read_write_byte(0x56);
+		cs_high(i);
+		DELAY(100); 
+	}
+	
+	/*************
+	通道映射 寄存器3
+	bit 15      0    ch-en
+	bit 13 12   000   setup-sel0
+	bit 9  5    00011  ain3
+	bit 4  0    10110  ref- 
+	
+	0x13
+	**********/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x13);
+		read_write_byte(0x80);
+		read_write_byte(0x76);
+		cs_high(i);
+		DELAY(100); 
+	}
+	
+	/***********
+	设置配置寄存器 0
+	bit 12   1  双极性输出
+	bit 5:4  00  外部电压源 
+	
+	0x20
+	*******/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x20);
+		read_write_byte(0x10);
+		read_write_byte(0x00);
+		cs_high(i);
+		DELAY(100); 
+	}
+	
+	/***********
+	滤波器设置 0
+	bit 15  0
+	bit 11 1
+	bit 10 8    110
+	bit 6 5   00
+	bit 4 0  10011 输出速率 10
+	
+	0x28
+	*******/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x28);
+		read_write_byte(0x0e);
+		read_write_byte(0x13);
+		cs_high(i);
+		DELAY(100); 
+	}
+
+	/***********
+	增益设置 0
+	
+	0x38
+	*******/
+	for(i = 0;i < 4;i ++)
+	{
+		cs_low(i);
+        DELAY(100); 
+		read_write_byte(0x38);
+		read_write_byte(0x55);
+		read_write_byte(0x55);
+		read_write_byte(0x55);
+		cs_high(i);
+		DELAY(100); 
+	}
+
+}
+
+unsigned char check(int i)
+{
+	unsigned char flag;
+	
+	cs_low(i);
+DELAY(50);
+	read_write_byte(0x40);
+	flag=read_write_byte(0x00);
+	cs_high(i);
+DELAY(50);
+	
+	return flag;
+} 
+
+void cs_low(int num)
+{
+	switch(num)
+	{
+		case 0:
+		{
+			nSS1 = 0;
+			break;
+		}
+		case 1:
+		{
+			nSS2 = 0;
+			break;
+		}
+		case 2:
+		{
+			nSS3 = 0;
+			break;
+		}
+		case 3:
+		{
+			nSS4 = 0;
+			break;
+		}
+		default: break;
+	}
+}
+
+void cs_high(int num)
+{
+	switch(num)
+	{
+		case 0:
+		{
+			nSS1 = 1;
+			break;
+		}
+		case 1:
+		{
+			nSS2 = 1;
+			break;
+		}
+		case 2:
+		{
+			nSS3 = 1;
+			break;
+		}
+		case 3:
+		{
+			nSS4 = 1;
+			break;
+		}
+		default: break;
+	}
+}
+
+void DELAY(unsigned int t) //t=1 15us
+{
+	unsigned int i,j;
+  	for(i=0;i<50;i++)
+  	{
+	  	for(j=0;j<t;j++)
+   		{
+	   		asm("nop");	
+    	}
+   } 	
+} 
---
+++ main.c
@@ -0,0 +1,405 @@
+#if defined(__dsPIC33F__)
+#include <p33Fxxxx.h>
+#elif defined(__PIC24H__)
+#include <p24hxxxx.h>
+#endif
+#include "timer.h"
+#include "adc.h"
+#include "sci.h"
+#include "AD7176.h" 
+#include "stdio.h"
+#include "collect.h"
+
+_FBS(0xCF);
+_FSS(0xCF);
+_FGS(0x07);
+_FOSCSEL(0xA2);  //Select HS without PLL
+_FOSC(0x46);  // HS oscilator, OSC2 is clock output
+_FWDT(0xDF);
+_FPOR(0xE7);
+//_FICD(0xC3);
+#define CLRWDT {__asm__ volatile ("CLRWDT");}
+
+#define COMM LATCbits.LATC14
+#define WORK LATCbits.LATC13
+#define STAT LATDbits.LATD0
+#define FAIL LATDbits.LATD11
+#define Nrest LATFbits.LATF6
+
+typedef union
+{ 
+   struct 
+   {
+        unsigned long AD_value;
+   };
+   struct   
+   {
+        unsigned char AD_1;
+        unsigned char AD_2;
+        unsigned char AD_3;
+        unsigned char AD_4;
+   };
+   struct   
+   {
+        unsigned int AD_11;
+        unsigned int AD_12;
+   };
+} AD_type;
+
+AD_type AD_buffer[4][4];
+AD_type AD_value[4][4];
+
+unsigned char flag_data=0,flag1=0;
+unsigned char work_enable=0;
+unsigned char capture_enable=0;
+unsigned char MAIN_ID=16;
+
+int count_reset = 0; 
+
+unsigned char send_data[200]={1,2,3,4,5,6,7,8,9,10};
+char send_ascii[250];
+char flag_ascii_or_bin = 'a';
+
+ 
+unsigned int Tick_60S=0; // 用来记录定时中断次数
+
+unsigned char uart2_enable = 0;//串口使能位
+unsigned char uart1_enable = 0;//网口使能位
+
+void UART2_Send(unsigned char str[], int len)
+{
+	int i;
+	for(i=0;i<len;i++)
+	{
+		U2TXREG = str[i];
+		while((U2STA&0x0100)==0x0000){asm("nop");}
+	}
+}
+
+void UART1_Send(unsigned char str[], int len)
+{
+	int i;
+	for(i=0;i<len;i++)
+	{
+		U1TXREG = str[i];
+		while((U1STA&0x0100)==0x0000){asm("nop");}
+	}
+}
+
+void __attribute__((interrupt,no_auto_psv)) _T6Interrupt(void)  // 1ms interrupt
+{
+	IFS2bits.T6IF = 0;
+	Tick_60S++;		
+
+    if(Tick_60S % 100 == 1)
+    {
+		capture_enable = 1;
+	}
+
+	if(Tick_60S >= 64000)
+	{
+		Tick_60S = 0;
+		count_reset ++;
+	}
+	if(count_reset >= 30)
+	{
+		Nrest=0;
+		DELAY(1000);
+		Nrest=1;
+		count_reset = 0;
+	} 		
+}
+
+/******
+网口中断
+**********/
+
+void __attribute__((interrupt,no_auto_psv)) _U1RXInterrupt(void)
+{
+	IFS0bits.U1RXIF = 0;
+	unsigned char data[17];
+	unsigned int UART_Timeout = 0;
+	unsigned char i = 0;
+	data[0] = U1RXREG;
+			 
+	while(data[i]!='E')
+	{
+		i++;
+		while((0==(U1STA&0x0001))&&(UART_Timeout<50000))
+		{UART_Timeout++;}
+		if(UART_Timeout>=50000)
+		{
+			data[i]='E'; // 如果接收超时，退出中断
+		}
+		else
+		{
+			data[i] = U1RXREG;
+		}
+		UART_Timeout = 0;
+	}
+	
+	if( (i==16)&&(data[2]==0X03)&&(data[3]==0X04)&&(data[0]='S') )
+	{	
+			count_reset = 0;
+		    flag_ascii_or_bin = 'b';
+			work_enable = 1;//握手指令中的ID与箱号一致时，才会开启该采集箱激频拾频
+			uart1_enable =1;
+			Tick_60S=0;			
+	}//if(i==16)
+	return;	
+}//
+
+
+
+
+
+/******
+外部串口中断
+
+
+**********/
+int start_judge = 0;
+
+void __attribute__((interrupt,no_auto_psv)) _U2RXInterrupt(void)
+{
+	unsigned int UART_Timeout = 0;
+	unsigned char i = 0;
+	unsigned char dat;
+	unsigned char receive_buf[60];
+	
+	IFS1bits.U2RXIF = 0;
+	dat = U2RXREG;
+	
+	if((start_judge == 0) && (dat == 'S'))
+    {
+		start_judge = 1;
+		receive_buf[0] = dat;
+    }
+	else if((start_judge == 0) && (dat == 'C'))
+    {
+		start_judge = 3;
+    }
+    else if(start_judge == 1)
+	{
+		while(receive_buf[i]!='E')
+		{
+			if(i == 0)
+			{
+				i = 1;
+				receive_buf[i] = dat;
+			}	
+			i++;
+			while((0==(U2STA&0x0001))&&(UART_Timeout<50000))
+			{UART_Timeout++;}
+			if(UART_Timeout>=50000)
+			{
+				receive_buf[i]='E'; // 如果接收超时，退出中断
+			}
+			else
+			{
+				receive_buf[i] = U2RXREG;
+			}
+			UART_Timeout = 0;
+		}	
+		start_judge = 0; 
+		if((i==16)&&(receive_buf[2]==0X01)&&(receive_buf[3]==0X02)) 
+		{	
+			work_enable = 1;//握手指令中的ID与箱号一致时，才会开启该采集箱激频拾频
+			uart2_enable =1;
+			flag_ascii_or_bin = 'b';	
+		}
+	}
+	
+	else if((start_judge == 0) && (dat == '5'))
+    {
+		start_judge = 2;
+		receive_buf[0] = dat;
+    }
+    else if(start_judge == 2)
+	{
+		while((receive_buf[i] != '5') || (i == 0))
+		{
+			if(i == 0)
+			{
+				i = 1;
+				receive_buf[i] = dat;
+			}
+			i++;
+			while((0==(U2STA&0x0001))&&(UART_Timeout<50000))
+			{UART_Timeout++;}
+			if(UART_Timeout>=50000)
+			{
+				receive_buf[i]='5'; // 如果接收超时，退出中断
+			}
+			else
+			{
+				receive_buf[i] = U2RXREG;
+			}
+			UART_Timeout = 0;
+		}	
+		start_judge = 0; 
+		if((i==49)&&(receive_buf[7]=='1')&&(receive_buf[10]=='2')) 
+		{	
+			work_enable = 1;//握手指令中的ID与箱号一致时，才会开启该采集箱激频拾频
+			uart2_enable =1;
+			flag_ascii_or_bin = 'a';	
+		}
+	}
+	else if(start_judge == 3)
+	{
+		start_judge = 0;
+		if(dat == 'Q')
+		{
+			IEC1bits.U2RXIE = 0; // Enable UART2 RX interrupt
+            IEC1bits.U2TXIE = 0;
+			IEC0bits.U1RXIE = 0; //  Enable UART1 RX interrupt
+			IEC0bits.U1TXIE = 0;
+			IEC2bits.T6IE = 0;
+			IEC2bits.C1IE=0;
+			C1INTEbits.RBIE=0;
+			while(1); //饿狗，让狗来重启
+		}
+	}	
+	return;	
+}
+
+int main()
+{
+	int i;
+	int k,j;
+	int n,m;
+	int s=0;
+	int temp1;
+    unsigned int humi_val_i,temp_val_i;
+	unsigned char error,checksum;
+    unsigned char read_temp;
+    unsigned char adc_temp[3];
+    unsigned char test[7] = {0x55,0x55,0x55,'t','e','s','t'};
+	unsigned int wait;
+    CLRWDT
+	OSCCON = 0x2200;
+
+    SPI_Init();
+    DELAY(5000);
+    AD7176set();
+	InitTimer6();  //// Timer6 提供0.1s中断定时
+    StartTimer6();
+	InitSCI();
+    s_connectionreset();
+     
+    TRISCbits.TRISC13 = 0;
+    TRISDbits.TRISD0 = 0;
+    TRISFbits.TRISF6 = 0; 
+    Nrest=1;
+    WORK = 1;
+    STAT = 0;
+
+	CLRWDT
+ 
+    //UART2_Send(test,7);
+	
+	while(1)
+	{
+		CLRWDT
+
+		if(capture_enable)
+		{
+			for(i = 0;i < 4;i ++)
+			{
+				while((!(check(i)&0x80)) && (wait < 80))
+				{wait ++;}
+
+				cs_low(i);
+                DELAY(50);
+
+	
+					read_write_byte(0x44);
+
+					adc_temp[2]=read_write_byte(0x00); 
+					adc_temp[1]=read_write_byte(0x00);
+					adc_temp[0]=read_write_byte(0x00);
+                    flag_data=read_write_byte(0x00);
+
+					if(!(flag_data&0x80)) //有数据转化成功
+					{
+						flag_data = flag_data&0x03;////通道0 返回通道号 
+						AD_buffer[i][flag_data].AD_3=adc_temp[2]; 
+						AD_buffer[i][flag_data].AD_2=adc_temp[1];
+						AD_buffer[i][flag_data].AD_1=adc_temp[0];
+						AD_buffer[i][flag_data].AD_4=0;	
+						Tick_60S = 0;
+					}
+					//AD_value[0].AD_value= AD_value[0].AD_value*0.9f+AD_buffer[0].AD_value*0.1f;
+	        	
+				cs_high(i);
+				DELAY(50); 
+				CLRWDT
+			}
+			capture_enable = 0;
+		}
+
+    	if(work_enable)
+		{
+			error=0;
+     		error+=s_measure((unsigned char*) &humi_val_i,&checksum,HUMI); 
+     		error+=s_measure((unsigned char*) &temp_val_i,&checksum,TEMP); 
+
+			if(error!=0)
+        		s_connectionreset(); 
+			else
+	    		error=0;
+
+			/*for(i = 0;i < 4;i ++)
+			{
+				test[i] = check(i);
+			}
+			UART1_Send(test,4);*/
+			
+			send_data[0]='S';
+			send_data[1]=1;
+			send_data[2]=2;	
+	
+			send_data[3] = 3;
+			send_data[4] =4;
+			send_data[5] = 5;
+			send_data[6] = 6;
+			send_data[7] = MAIN_ID;
+	        s=8;
+
+			for(k = 0;k < 4;k ++)
+			{
+				send_data[s] = k;s++;
+				for(m = 0;m < 4;m ++)
+				{   
+					send_data[s] = AD_buffer[k][m].AD_3;s++;
+					send_data[s] = AD_buffer[k][m].AD_2;s++;
+					send_data[s] = AD_buffer[k][m].AD_1;s++; 
+				}
+			}
+			
+			send_data[s] = 0x00;s++;
+			send_data[s] = 0x00;s++;
+
+			send_data[s] = humi_val_i>>8;s++;
+			send_data[s] = humi_val_i;s++;
+			send_data[s] = temp_val_i>>8;s++;
+     		send_data[s] = temp_val_i;s++;
+
+			send_data[s]=  'E';s++;
+
+			if(uart2_enable ==1)
+			{
+				UART2_Send(send_data,s);
+			}
+			if(uart1_enable ==1)
+        	{
+        		UART1_Send(send_data,s);
+        	}	
+            
+			STAT = ~STAT;
+			work_enable = 0;
+        }      
+    }    
+	return 0;
+}
+
